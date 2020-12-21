bda986bd12ef95f4167628dcbec2079e641ae015
Cara Zellner <carajeanine@gmail.com>
2020-12-21 16:35:22 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -3,8 +3,8 @@ class Project
   attr_accessor :name
   attr_reader :backers 
   
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title = title
     @backers = []
   end
 end
